d247be94249e7290ea1dee7387f368ba15280d0f
audreyroch <audreyroch.houssou@gmail.com>
2025-08-21 13:15:52 +0200

    Add orchestrator wizards for service and environment

--- partenaires/bibind_portal/__manifest__.py
+++ partenaires/bibind_portal/__manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name': 'Bibind Portal',
-    'version': '0.1',
-    'summary': 'Customer portal for managing services',
-    'author': 'Bibind',
-    'depends': ['portal', 'web', 'mail', 'bus', 'bibind_core'],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/rules.xml',
-        'views/portal_menu.xml',
+    "name": "Bibind Portal",
+    "version": "0.1",
+    "summary": "Customer portal for managing services",
+    "author": "Bibind",
+    "depends": ["portal", "web", "mail", "bus", "bibind_core"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/rules.xml",
+        "views/portal_menu.xml",
+        "views/wizards.xml",
     ],
-    'application': False,
-    'installable': True,
+    "application": False,
+    "installable": True,
 }
---
+++ partenaires/bibind_portal/wizards/__init__.py
@@ -0,0 +1,3 @@
+from . import create_service_wizard
+from . import create_environment_wizard
+from . import post_deploy_wizard
