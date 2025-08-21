1150b9b68f86d4955200daee112e071856112ae2
audreyroch <audreyroch.houssou@gmail.com>
2025-08-21 16:04:59 +0200

    feat(project): add backlog and sprint planning

--- partenaires/bibind_portal_projects/wizards/sprint_plan.py
+++ partenaires/bibind_portal_projects/wizards/sprint_plan.py
@@ -2,33 +2,34 @@ from odoo import api, fields, models
 
 
 class SprintPlanWizard(models.TransientModel):
-    _name = 'kb.sprint.plan.wizard'
-    _description = 'Sprint Planning Wizard'
+    _name = "kb.sprint.plan.wizard"
+    _description = "Sprint Planning Wizard"
 
     project_id = fields.Many2one(
-        'project.project', required=True, default=lambda self: self.env.context.get('default_project_id')
+        "project.project",
+        required=True,
+        default=lambda self: self.env.context.get("default_project_id"),
     )
     date_start = fields.Date(required=True)
     date_end = fields.Date(required=True)
     capacity_hours = fields.Float()
     task_ids = fields.Many2many(
-        'project.task',
-        string='Stories',
+        "project.task",
+        string="Stories",
         domain="[('project_id','=',project_id), ('issue_type','=','story'), ('sprint_id','=', False)]",
     )
 
     def action_plan(self):
         self.ensure_one()
-        name = self.env.context.get('default_name') or f"Sprint {self.date_start}"
-        sprint = self.env['kb.sprint'].create(
+        name = self.env.context.get("default_name") or f"Sprint {self.date_start}"
+        sprint = self.env["kb.sprint"].create(
             {
-                'project_id': self.project_id.id,
-                'name': name,
-                'date_start': self.date_start,
-                'date_end': self.date_end,
-                'capacity_hours': self.capacity_hours,
+                "project_id": self.project_id.id,
+                "name": name,
+                "date_start": self.date_start,
+                "date_end": self.date_end,
+                "capacity_hours": self.capacity_hours,
             }
         )
-        self.task_ids.write({'sprint_id': sprint.id})
-        return {'type': 'ir.actions.act_window_close'}
-
+        self.task_ids.write({"sprint_id": sprint.id})
+        return {"type": "ir.actions.act_window_close"}
